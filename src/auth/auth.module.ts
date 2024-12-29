e8c6782f351cd965b56e07bc06f2853681c59160
nowgnoesiohc <csw6203@naver.com>
2024-12-29 17:09:30 +0900

    feat : implement user login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User, UserSchema } from '../user/schemas/user.schema';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'defaultSecret',
+      signOptions: { expiresIn: '1h' },
+    }),
   ],
   providers: [AuthService],
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,28 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/user/schemas/user.schema';
 import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
 
+  /**
+   * 회원가입
+   * @param signupDto - username과 password를 포함
+   * @returns 생성된 사용자 정보
+   */
   async signup(SignupDto: SignupDto): Promise<User> {
     const { username, password } = SignupDto;
 
@@ -28,5 +42,31 @@ export class AuthService {
     });
 
     return newUser.save();
+  }
+
+  /**
+   * 로그인
+   * @param loginDto - username과 password를 포함
+   * @returns JWT 액세스 토큰
+   */
+  async login(loginDto: LoginDto): Promise<{ accessToken: string }> {
+    const { username, password } = loginDto;
+
+    const user = await this.userModel.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // 비밀번호 검증
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // JWT 생성
+    const payload = { username, sub: user._id };
+    const accessToken = this.jwtService.sign(payload);
+    // NOTE : signAsync
+    return { accessToken };
   }
 }
