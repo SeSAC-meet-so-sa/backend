73a0e944b812b9fde19321ab3dc759faed5cc12c
nowgnoesiohc <csw6203@naver.com>
2024-12-30 17:05:36 +0900

    chore : update config for CORS

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*'],
-    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+    origin: ['https://api.meet-da.site'], // 허용할 도메인
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
