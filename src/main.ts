d44b9563ae7df9623492046019b756cdc0562798
nowgnoesiohc <csw6203@naver.com>
2025-01-13 21:36:20 +0900

    feat : apply class-validator + class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://api.meet-da.site', 'http://localhost:3000'], // 허용할 도메인
+    origin: [
+      'https://api.meet-da.site',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ], // 허용할 도메인
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 
