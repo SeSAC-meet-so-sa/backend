4527cd757367452aa3fc72588d1d2fbc31bf1335
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2025-01-13 16:17:21 +0900

    fix : enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://api.meet-da.site', 'http://localhost:3000'], // 허용할 도메인
+    origin: ['https://api.meet-da.site', 'http://localhost:3000', 'http://localhost:5173'], // 허용할 도메인
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
